36892204a59f43800099034150b6bb9ef31a48b9
AbelRamirez03 <abel.ramirez76@uabc.edu.mx>
2025-12-02 19:48:49 -0800

    Modificacion de grades.module y service- Auditorias

--- Back-end/src/grades/grades.module.ts
+++ Back-end/src/grades/grades.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { GradesController } from './grades.controller';
 import { GradesService } from './grades.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ActivityLogModule } from '../activityTimeline/activityTimeline.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ActivityLogModule],
   controllers: [GradesController],
   providers: [GradesService],
   exports: [GradesService]
